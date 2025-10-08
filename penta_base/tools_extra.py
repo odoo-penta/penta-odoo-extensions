53b81d94d0fe7395092911e5bc8d1f3a2921ef90
ViniPenta <apineda@pentalab.tech>
2025-10-08 14:50:02 -0500

    [FIX] pruebas

--- penta_base/tools_extra.py
+++ penta_base/tools_extra.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import re
 import unicodedata
-from lxml import etree as LET
+from lxml import etree
 
 
 def remove_accents(text):
@@ -39,7 +39,7 @@ def format_invoice_number(text):
 
 def xml_element(parent , tag, text=None, **attrs):
         """Helper para crear un elemento XML con atributos opcionales y texto."""
-        el = LET.SubElement(parent, tag, **{k: str(v) for k, v in attrs.items() if v is not None})
+        el = etree.SubElement(parent, tag, **{k: str(v) for k, v in attrs.items() if v is not None})
         if text is not None:
             el.text = str(text)
         return el
@@ -78,3 +78,77 @@ def doc_type_code(latam_doc_type):
         if val:
             return str(val)
     return ""
+
+def remove_control_characters(byte_node):
+    """
+    The characters to be escaped are the control characters #x0 to #x1F and #x7F (most of which cannot appear in XML)
+    [...] XML processors must accept any character in the range specified for Char:
+    `Char	   :: =   	#x9 | #xA | #xD | [#x20-#xD7FF] | [#xE000-#xFFFD] | [#x10000-#x10FFFF]`
+    source:https://www.w3.org/TR/xml/
+    """
+    return re.sub(
+        '[^'
+        '\u0009'
+        '\u000A'
+        '\u000D'
+        '\u0020-\uD7FF'
+        '\uE000-\uFFFD'
+        '\U00010000-\U0010FFFF'
+        ']'.encode(),
+        b'',
+        byte_node,
+    )
+
+def cleanup_xml_node(xml_node_or_string, remove_blank_text=True, remove_blank_nodes=True, indent_level=0, indent_space="  "):
+    """Clean up the sub-tree of the provided XML node.
+
+    If the provided XML node is of type:
+    - etree._Element, it is modified in-place.
+    - string/bytes, it is first parsed into an etree._Element
+    :param xml_node_or_string (etree._Element, str): XML node (or its string/bytes representation)
+    :param remove_blank_text (bool): if True, removes whitespace-only text from nodes
+    :param remove_blank_nodes (bool): if True, removes leaf nodes with no text (iterative, depth-first, done after remove_blank_text)
+    :param indent_level (int): depth or level of node within root tree (use -1 to leave indentation as-is)
+    :param indent_space (str): string to use for indentation (use '' to remove all indentation)
+    :returns (etree._Element): clean node, same instance that was received (if applicable)
+    """
+    xml_node = xml_node_or_string
+    # Convert str/bytes to etree._Element
+    if isinstance(xml_node, str):
+        xml_node = xml_node.encode()  # misnomer: fromstring actually reads bytes
+    if isinstance(xml_node, bytes):
+        parser = etree.XMLParser(recover=True, resolve_entities=False)
+        xml_node = etree.fromstring(remove_control_characters(xml_node), parser=parser)
+
+    # Process leaf nodes iteratively
+    # Depth-first, so any inner node may become a leaf too (if children are removed)
+    def leaf_iter(parent_node, node, level):
+        for child_node in node:
+            leaf_iter(node, child_node, level if level < 0 else level + 1)
+
+        # Indentation
+        if level >= 0:
+            indent = '\n' + indent_space * level
+            if not node.tail or not node.tail.strip():
+                node.tail = '\n' if parent_node is None else indent
+            if len(node) > 0:
+                if not node.text or not node.text.strip():
+                    # First child's indentation is parent's text
+                    node.text = indent + indent_space
+                last_child = node[-1]
+                if last_child.tail == indent + indent_space:
+                    # Last child's tail is parent's closing tag indentation
+                    last_child.tail = indent
+        
+        # Removal condition: node is leaf (not root nor inner node)
+        if parent_node is not None and len(node) == 0:
+            if remove_blank_text and node.text is not None and not node.text.strip():
+                # node.text is None iff node.tag is self-closing (text='' creates closing tag)
+                node.text = ''
+                print("Entro op1")
+            if remove_blank_nodes and not (node.text or ''):
+                parent_node.remove(node)
+                print("Entro op2")
+
+    leaf_iter(None, xml_node, indent_level)
+    return xml_node

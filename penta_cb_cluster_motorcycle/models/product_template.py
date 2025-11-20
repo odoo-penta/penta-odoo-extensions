a44c6205d11021a132a3267c4042e7c74b7533b2
peterpentalab <pmendez@pentalab.tech>
2025-11-20 15:34:32 -0500

    [FIX]  Update field boolean is_ensabled

--- penta_cb_cluster_motorcycle/models/product_template.py
+++ penta_cb_cluster_motorcycle/models/product_template.py
@@ -3,5 +3,7 @@ from odoo import fields, models
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    is_ensabled = fields.Boolean(string=_("Ensabled product ?"),
-                                 help=_("Field that indicate if the product is ensabled"))
+    is_ensabled = fields.Boolean(
+        string="Producto ensamblado?",
+        help="Indica si el producto es ensamblado para aplicar ICE."
+    )

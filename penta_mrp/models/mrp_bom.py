89dc0897ac5c08c6db13087ee35602cbc9f52143
ViniPenta <apineda@pentalab.tech>
2026-01-08 09:26:58 -0500

    [FIX] ajuste calculo costo MP + MOD

--- penta_mrp/models/mrp_bom.py
+++ penta_mrp/models/mrp_bom.py
@@ -2,6 +2,7 @@
 
 from odoo import models,  _
 
+
 class MrpBom(models.Model):
     _inherit = 'mrp.bom'
     
@@ -9,30 +10,28 @@ class MrpBom(models.Model):
         """
         Obtiene el costo unitario de la lista de materiales (BOM).
         """
-        result = {}
-        for bom in self:
-            total = 0.0
+        self.ensure_one()
+        total = 0.0
+        
 
-            # Costos de materiales
-            for line in bom.bom_line_ids:
-                total += line.standard_price * line.product_qty
+        # Costos de materiales
+        for line in self.bom_line_ids:
+            total += line.standard_price * line.product_qty
 
-            # Costos de operaciones
-            for operation in bom.operation_ids:
-                duration_expected = (
-                    operation.workcenter_id._get_expected_duration(operation) +
-                    (operation.time_cycle * 100 / operation.workcenter_id.time_efficiency)
-                )
+        # Costos de operaciones
+        for operation in self.operation_ids:
+            workcenter = operation.workcenter_id
+            time_cycle_hours = operation.time_cycle or 0.0
+            efficiency = workcenter.time_efficiency or 100.0
+            
+            duration_expected = (time_cycle_hours * 60.0) / (efficiency / 100)
 
-                cost_hour = (
-                    operation.workcenter_id.costs_hour +
-                    operation.workcenter_id.workcenter_team_costs_hour
-                )
+            cost_hour = (
+                workcenter.costs_hour +
+                workcenter.workcenter_team_costs_hour
+            )
 
-                total += (duration_expected / 60.0) * cost_hour
+            total += (duration_expected / 60.0) * cost_hour
 
-            # Costo unitario
-            product_qty = bom.product_qty or 1.0
-            result[bom.id] = total / product_qty
-
-        return result
+        # Costo unitario
+        return total / (self.product_qty or 1.0)

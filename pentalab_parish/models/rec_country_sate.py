d059d339e0832a4752b911262e2bc9e7ed1d5bee
CarlosCondor99 <ccondorp@pentalab.tech>
2026-01-29 16:55:38 -0500

    [IMP] pentalab_parish

--- pentalab_parish/models/rec_country_sate.py
+++ pentalab_parish/models/rec_country_sate.py
@@ -3,4 +3,4 @@ from odoo import models, fields, api, _
 class ResCountryState(models.Model):
     _inherit = 'res.country.state'
 
-    region_id = fields.Many2one('res.country.state.region', string='Region', required=True, index=True)
+    region_id = fields.Many2one('res.country.state.region', string='Region', index=True)

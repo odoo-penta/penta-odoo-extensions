bb428a8c04afa8e550445d1a734aabd3fa52823f
ViniPenta <apineda@pentalab.tech>
2026-01-30 13:02:09 -0500

    [FIX] formato manifest

--- penta_base/__manifest__.py
+++ penta_base/__manifest__.py
@@ -23,11 +23,12 @@
     'category': 'Tools/Tools',
     'version': '18.0.0.1',
     'depends': [
-        'base', 'purchase', 'stock', 'account'
+        'base',
+        'purchase',
+        'stock',
+        'account',
     ],
     'data': [
-    ],
-    'demo': [
     ],
     "post_init_hook": "post_init_hook",
     'installable': True,

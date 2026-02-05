56bfd9ff04ee768eb0db34b0ae28548ed4015cdb
ViniPenta <apineda@pentalab.tech>
2026-02-05 10:53:40 -0500

    [ADD] module penta purchase no portal rfq

---
+++ penta_purchase_no_portal_rfq/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+# Author      : PentaLab (<https://pentalab.tech>)
+# Copyright(c): 2025
+# All Rights Reserved.
+#
+# This module is copyright property of the author mentioned above.
+# You can`t redistribute it and/or modify it.
+#
+#################################################################################
+{
+    "name": "Penta Correo sin cotizacion",
+    "summary": "Elimina acceso portar de cotización",
+    "description": """
+        Al enviar correo al proveedor de cotizacion no muestra boton de Ver cotizacion (Portal).
+    """,
+    'author': 'PentaLab',
+    'maintainer': 'PentaLab',
+    'contributors': ['AntonyPineda <vini16.av@gmail.com>'],
+    'website': 'https://pentalab.tech/',
+    "license": "LGPL-3",
+    'category': 'Uncategorized',
+    "version": "18.0.0.0.0",
+    "depends": [
+        'purchase',
+    ],
+    "data": [
+        
+    ],
+    "installable": True,
+    "application": False,
+}

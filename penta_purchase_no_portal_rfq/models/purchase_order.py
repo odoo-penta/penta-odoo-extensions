fa179b49d1e48df3702f47f3eedff342fc89e091
ViniPenta <apineda@pentalab.tech>
2026-02-05 11:02:33 -0500

    [FIX] metodo _notify_get_recipients_groups

--- penta_purchase_no_portal_rfq/models/purchase_order.py
+++ penta_purchase_no_portal_rfq/models/purchase_order.py
@@ -7,20 +7,4 @@ class PurchaseOrder(models.Model):
     _name = 'purchase.order'
 
     def _notify_get_recipients_groups(self, message, model_description, msg_vals=None):
-        groups = super()._notify_get_recipients_groups(
-            message, model_description, msg_vals=msg_vals
-        )
-
-        if not self:
-            return groups
-
-        self.ensure_one()
-
-        # Quitar botón SOLO en cotizaciones (RFQ)
-        if self.state in ('draft', 'sent'):
-            for group in groups:
-                if group[0] == 'portal_customer':
-                    group[2].pop('button_access', None)
-                    group[2].pop('actions', None)
-
-        return groups
+        return super()._notify_get_recipients_groups(message, model_description, msg_vals=msg_vals)

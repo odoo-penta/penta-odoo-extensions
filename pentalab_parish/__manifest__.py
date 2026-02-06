ef4f69884d3585fe30221da13581337d4a20674d
JorgeArmas246 <jarmas@pentalab.tech>
2026-02-06 12:21:15 -0500

    [IMP] penta_mrp_auto_process_all: cambio de funcionalidad para el proceso de fabricacion por lote,[IMP] pentalab_parish cambio

--- pentalab_parish/__manifest__.py
+++ pentalab_parish/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Partner Parroquias',
-    'version': '18.0.1.0.2',
+    'version': '18.0.1.0.3',
     'category': 'Contacts',
     'summary': 'Agrega un campo de parroquias en los contactos',
     'author': 'PentaLab',
